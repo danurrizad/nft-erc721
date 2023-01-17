b95a768718446df6f69f896775a90d92fe557631
danurrizad <danurrizad242@gmail.com>
2023-01-17 10:26:16 +0700

    add web3 require

--- block.js
+++ block.js
@@ -3,6 +3,7 @@ const { async } = require("regenerator-runtime");
 var account = null;
 
 async function Connect(){
+        Web3 = require('web3')
         await window.ethereum.send('eth_requestAccounts');
         window.web3 = new Web3(window.ethereum);
 
